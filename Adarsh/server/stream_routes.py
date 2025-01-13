0fc363f45c84ab5e0b04f64843a46e7e6721f21e
Shubham Gote <0shubhamgote0@gmail.com>
2025-01-13 22:29:33 +0530

    Update stream_routes.py

--- Adarsh/server/stream_routes.py
+++ Adarsh/server/stream_routes.py
@@ -20,23 +20,28 @@ from Adarsh.vars import Var
 
 routes = web.RouteTableDef()
 
+# Serve the homepage HTML
 @routes.get("/", allow_head=True)
-async def root_route_handler(_):
-    return web.json_response(
-        {
-            "server_status": "running",
-            "uptime": get_readable_time(time.time() - StartTime),
-            "telegram_bot": "@" + StreamBot.username,
-            "connected_bots": len(multi_clients),
-            "loads": dict(
-                ("bot" + str(c + 1), l)
-                for c, (_, l) in enumerate(
-                    sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
-                )
-            ),
-            "version": __version__,
-        }
-    )
+@aiohttp_jinja2.template('home.html')
+
+# @routes.get("/", allow_head=True)
+# async def root_route_handler(_):
+#     return web.json_response(
+#         {
+#             "server_status": "running",
+#             ""
+#             "uptime": get_readable_time(time.time() - StartTime),
+#             "telegram_bot": "@" + StreamBot.username,
+#             "connected_bots": len(multi_clients),
+#             "loads": dict(
+#                 ("bot" + str(c + 1), l)
+#                 for c, (_, l) in enumerate(
+#                     sorted(work_loads.items(), key=lambda x: x[1], reverse=True)
+#                 )
+#             ),
+#             "version": __version__,
+#         }
+#     )
 
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
